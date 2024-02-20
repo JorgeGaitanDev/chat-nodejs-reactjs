f127cb7caf13d3faeda7cdde82dc28ded75a8642
JorgeGaitanDev <jgaitanh12@gmail.com>
2024-02-20 01:36:08 -0500

    updated ChatsPage

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,5 +1,17 @@
-const ChatsPage = () => {
-    return <>chats...</>
+import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from 'react-chat-engine-advanced'
+
+const ChatsPage = (props) => {
+    const chatProps = useMultiChatLogic(
+        '7f60d16d-eb90-4224-922d-6b640ff0bd8f', 
+        props.user.username, 
+        props.user.secret
+    );
+    return (
+        <div style={{ height: '100vh' }}>
+            <MultiChatSocket {...chatProps} />
+            <MultiChatWindow {...chatProps} style={{ height: '100%'}} />    
+        </div>
+    )
 }
 
 export default ChatsPage
